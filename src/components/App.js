0c943853c18eec27e249246f86bb68e49a8649e6
Marion <marion.carambula@modop.com>
2018-07-09 14:25:24 -0500

    Replying tweets
    Now, we can reply to a specific tweet.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import LoadingBar from 'react-redux-loading';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
 
 class App extends Component {
     componentDidMount() {
@@ -13,7 +14,7 @@ class App extends Component {
         return (
             <div>
                 <LoadingBar />
-                {this.props.loading ? null : <NewTweet />}
+                {this.props.loading ? null : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}} />}
             </div>
         )
     }
